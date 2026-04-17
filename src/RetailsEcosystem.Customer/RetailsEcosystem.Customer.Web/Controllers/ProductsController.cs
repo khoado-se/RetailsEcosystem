09f5ee5bc43f8c3f18ae7832f9277119f83a0dff
Khoa Do <dodangkhoaa1@gmail.com>
2026-04-17 14:57:14 +0700

    feat: create home page for customer web

---
+++ src/RetailsEcosystem.Customer/RetailsEcosystem.Customer.Web/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetailsEcosystem.Customer.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
